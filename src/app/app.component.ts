db3f565617454da3840b17c82d4bcb11eb9677d0
Guillaume1979 <guibetguillaume@gmail.com>
2024-10-16 23:48:28 +0200

    Refactor: replace the player number by X and O symbol, and simplify the html of the cells

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -9,19 +9,19 @@ import { RouterOutlet } from '@angular/router';
     <h1>Joue au morpion</h1>
     <h2>Au tour du joueur {{ activePlayer }}</h2>
     <div class="line">
-      <button class="cell" (click)="play(0,0)">{{ grid[0][0] === 0 ? '' : grid[0][0] }}</button>
-      <button class="cell" (click)="play(0,1)">{{ grid[0][1] === 0 ? '' : grid[0][1] }}</button>
-      <button class="cell" (click)="play(0,2)">{{ grid[0][2] === 0 ? '' : grid[0][2] }}</button>
+      <button class="cell" (click)="play(0,0)">{{ displaySymbolByPlayer(grid[0][0]) }}</button>
+      <button class="cell" (click)="play(0,1)">{{ displaySymbolByPlayer(grid[0][1]) }}</button>
+      <button class="cell" (click)="play(0,2)">{{ displaySymbolByPlayer(grid[0][2]) }}</button>
     </div>
     <div class="line">
-      <button class="cell" (click)="play(1,0)">{{ grid[1][0] === 0 ? '' : grid[1][0] }}</button>
-      <button class="cell" (click)="play(1,1)">{{ grid[1][1] === 0 ? '' : grid[1][1] }}</button>
-      <button class="cell" (click)="play(1,2)">{{ grid[1][2] === 0 ? '' : grid[1][2] }}</button>
+      <button class="cell" (click)="play(1,0)">{{ displaySymbolByPlayer(grid[1][0]) }}</button>
+      <button class="cell" (click)="play(1,1)">{{ displaySymbolByPlayer(grid[1][1]) }}</button>
+      <button class="cell" (click)="play(1,2)">{{ displaySymbolByPlayer(grid[1][2]) }}</button>
     </div>
     <div class="line">
-      <button class="cell" (click)="play(2,0)">{{ grid[2][0] === 0 ? '' : grid[2][0] }}</button>
-      <button class="cell" (click)="play(2,1)">{{ grid[2][1] === 0 ? '' : grid[2][1] }}</button>
-      <button class="cell" (click)="play(2,2)">{{ grid[2][2] === 0 ? '' : grid[2][2] }}</button>
+      <button class="cell" (click)="play(2,0)">{{ displaySymbolByPlayer(grid[2][0]) }}</button>
+      <button class="cell" (click)="play(2,1)">{{ displaySymbolByPlayer(grid[2][1]) }}</button>
+      <button class="cell" (click)="play(2,2)">{{ displaySymbolByPlayer(grid[2][2]) }}</button>
     </div>
     @if (hasError) {
       <p class="forbidden">Coup interdit !</p>
@@ -33,101 +33,109 @@ import { RouterOutlet } from '@angular/router';
 
   `,
   styles: [`
-  :host {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
+    :host {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    }
 
-  .line {
-    display: flex;
-  }
+    .line {
+      display: flex;
+    }
 
-  button:not(.new) {
-    width: 50px;
-    height: 50px;
-  }
+    button:not(.new) {
+      width: 50px;
+      height: 50px;
+    }
 
-  button.new {
-    padding: 20px;
-    background: bisque;
-    border: none;
-    border-radius: 5px;
-    box-shadow: 2px 2px 5px 1px dimgrey;
-  }
+    button.new {
+      padding: 20px;
+      background: bisque;
+      border: none;
+      border-radius: 5px;
+      box-shadow: 2px 2px 5px 1px dimgrey;
+    }
 
-  .forbidden {
-    color: red;
-    font-size: 2rem;
-    font-weight: bold;
-  }
+    .forbidden {
+      color: red;
+      font-size: 2rem;
+      font-weight: bold;
+    }
 
-  .winner {
-    color: green;
-    font-size: 5rem;
-    font-weight: bold;
-  }
+    .winner {
+      color: green;
+      font-size: 5rem;
+      font-weight: bold;
+    }
   `],
 })
 export class AppComponent {
   title = 'tic-tac-toe';
   activePlayer = 1;
-  grid: number [][] = [[0,0,0],[0,0,0],[0,0,0]];
+  grid: number [][] = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
   hasError = false
   winner = 0;
 
   play(x: number, y: number) {
-    if(this.winner) return;
+    if (this.winner) return;
     const player = this.activePlayer;
-    if (this.grid[x][y]!== 0) {
+    if (this.grid[x][y] !== 0) {
       this.displayError();
       return;
     }
     this.grid[x][y] = player;
     const winner = this.checkWin();
-    if(winner){
+    if (winner) {
       this.hasWon(winner);
       return;
     }
     this.activePlayer = this.activePlayer === 1 ? 2 : 1;
   }
 
-  startNewGame(){
+  startNewGame() {
     this.winner = 0;
     this.activePlayer = 1;
-    this.grid = [[0,0,0],[0,0,0],[0,0,0]];
+    this.grid = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+  }
+
+  displaySymbolByPlayer(player: number) {
+    return player === 0 ? '' : player === 1 ? 'X' : 'O';
   }
 
   private displayError() {
     this.hasError = true;
-    setTimeout(() => { this.hasError = false; }, 1000);
+    setTimeout(() => {
+      this.hasError = false;
+    }, 1000);
   }
 
   private checkWin() {
     const winningCombinations = [
-          [ [0,0], [0,1], [0,2] ],
-          [ [1,0], [1,1], [1,2] ],
-          [ [2,0], [2,1], [2,2] ],
-          [ [0,0], [1,0], [2,0] ],
-          [ [0,1], [1,1], [2,1] ],
-          [ [0,2], [1,2], [2,2] ],
-          [ [0,0], [1,1], [2,2] ],
-          [ [0,2], [1,1], [2,0] ]
-        ];
+      [[0, 0], [0, 1], [0, 2]],
+      [[1, 0], [1, 1], [1, 2]],
+      [[2, 0], [2, 1], [2, 2]],
+      [[0, 0], [1, 0], [2, 0]],
+      [[0, 1], [1, 1], [2, 1]],
+      [[0, 2], [1, 2], [2, 2]],
+      [[0, 0], [1, 1], [2, 2]],
+      [[0, 2], [1, 1], [2, 0]]
+    ];
 
-        for (const combination of winningCombinations) {
-          const [a, b, c] = combination;
-          if (this.grid[a[0]][a[1]] !== 0 &&
-              this.grid[a[0]][a[1]] === this.grid[b[0]][b[1]] &&
-              this.grid[a[0]][a[1]] === this.grid[c[0]][c[1]]) {
-            return this.grid[a[0]][a[1]];
-          }
-        }
+    for (const combination of winningCombinations) {
+      const [a, b, c] = combination;
+      if (this.grid[a[0]][a[1]] !== 0 &&
+        this.grid[a[0]][a[1]] === this.grid[b[0]][b[1]] &&
+        this.grid[a[0]][a[1]] === this.grid[c[0]][c[1]]) {
+        return this.grid[a[0]][a[1]];
+      }
+    }
 
-        return 0;
+    return 0;
   }
 
   private hasWon(player: number) {
     this.winner = player;
   }
 }
+
+//todo ajouter un restart au cours d'une partie et une fin quand personne ne gagne
